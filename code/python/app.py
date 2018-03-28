cbfdac8dc51f674cd5ef3f7525085f177c78bb25
wangying <wangying>
2018-03-28 14:13:31 +0800

    修改app.py

--- code/python/app.py
+++ code/python/app.py
@@ -5,10 +5,6 @@ from flask import Flask, url_for
 from flask_sqlalchemy import SQLAlchemy
 from yaml import load
 
-app = Flask(__name__) # EmbryoAI系统Flask APP
-db = SQLAlchemy(app) # 此APP要用到的数据库连接，由ORM框架SQLAlchemy管理
-logger = app.logger # 日志对象
-
 def read_yml_config(filename='configuration.yml'):
     '''从yaml文件中读取配置'''
     with open(filename, 'r') as fn:
@@ -17,11 +13,17 @@ def read_yml_config(filename='configuration.yml'):
 def init_config(conf):
     '''初始化app的基本配置'''
     # 数据库连接字符串
-    app.config['SQLALCHEMY_DATABASE_URI'] = config['SQLALCHEMY_DATABASE_URI'] if \
+    app.config['SQLALCHEMY_DATABASE_URI'] = conf['SQLALCHEMY_DATABASE_URI'] if \
         'SQLALCHEMY_DATABASE_URI' in conf else 'mysql+pymysql://root:@localhost/embryoai_system?charset=utf8'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # 返回的JSON数据保持原编码方式
     app.config['JSON_AS_ASCII'] = False
+
+app = Flask(__name__) # EmbryoAI系统Flask APP
+conf = read_yml_config()
+init_config(conf)
+db = SQLAlchemy(app) # 此APP要用到的数据库连接，由ORM框架SQLAlchemy管理
+logger = app.logger # 日志对象
 
 def init_logger():
     '''初始化日志的基本配置'''
@@ -42,8 +44,6 @@ def add_all_controller():
 
 if __name__=='__main__':
     init_logger()
-    conf = read_yml_config()
-    init_config(conf)
     port = conf['PORT'] if 'PORT' in conf else 5001 # app启动侦听的端口号
     debug = conf['DEBUG'] if 'DEBUG' in conf else False # 是否开启debug模式
     threaded = conf['THREADED'] if 'THREADED' in conf else True # 是否开启多线程模式
