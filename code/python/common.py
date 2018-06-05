5f3e48f5bdf4f4d3829ee4d4ebc4b354de79a958
王滢 <wangying@wangying.local>
2018-06-05 14:35:58 +0800

    refine some bugs

--- code/python/common.py
+++ code/python/common.py
@@ -15,7 +15,7 @@ def cross_domain(func):
         response = make_response(func(*args, **kwargs))
         response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
-        rst.headers['Access-Control-Allow-Headers'] = 'Referer,Accept,Origin,User-Agent'
+        response.headers['Access-Control-Allow-Headers'] = 'Referer,Accept,Origin,User-Agent'
         return response
     return allow_origin
 
