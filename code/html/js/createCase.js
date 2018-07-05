86854d4b1eda5bb82ac0ee97c3de2afe462a9967
luotengke <luotengke@qq.com>
2018-07-05 16:54:51 +0800

    添加了病历创建的页面（creartCase.html）

---
+++ code/html/js/createCase.js
@@ -0,0 +1,50 @@
+layui.use(['form', 'jquery', 'laydate', 'table', 'layer', 'element'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    var layer = layui.layer;
+	var laydate = layui.laydate;
+
+	// 培养箱选择
+	$('.incubator').on('click','span',function(){
+		if($(this).hasClass('active')){
+			$(this).removeClass('active');
+		}else{
+			$(this).siblings('span').removeClass('active');
+			$(this).addClass('active');
+		}
+	})
+	// 培养皿选择
+	$('.dish').on('click','span',function(){
+		if($(this).hasClass('active')){
+			$(this).removeClass('active');
+		}else{
+			$(this).addClass('active');
+		}
+	})	
+	
+	//日期
+	laydate.render({
+			elem: '#birth',
+			format: 'yyyy/MM/dd ',
+			max: 0
+	});
+	laydate.render({
+			elem: '#get',
+			format: 'yyyy/MM/dd ',
+			max: 0
+	});
+	laydate.render({
+			elem: '#iui',
+			format: 'yyyy/MM/dd ',
+			max: 0
+	});
+	
+	  //自定义验证规则例子
+  form.verify({
+    card: function(value){
+      if(value.length != 18){
+        return '身份证号长度应为18位';
+      }
+    }    
+  });
+})
