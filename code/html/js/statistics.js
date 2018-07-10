e904a04a9572e7cee518f9c1a8a0d74749e1abb9
luotengke <luotengke@qq.com>
2018-07-10 13:58:04 +0800

    重新更新了一下代码

---
+++ code/html/js/statistics.js
@@ -0,0 +1,7 @@
+layui.use(['form', 'jquery', 'laydate', 'table', 'layer', 'element'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    var layer = layui.layer;
+	var laydate = layui.laydate;
+
+})
