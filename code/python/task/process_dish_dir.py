bcfec48f51b2486d6a899a6ddd767606f9cc05e3
王滢 <wangying@wangying.local>
2018-07-13 13:03:30 +0800

    process_dish_dir.py update

---
+++ code/python/task/process_dish_dir.py
@@ -0,0 +1,37 @@
+# -*- coding: utf8 -*-
+
+from task.TimeSeries import TimeSeries
+import os
+
+
+def process_dish(path):
+    from functools import partial
+    dish_path = path + f'DISH{dish_info.index}' + os.path.sep
+    try:
+        last_op = open(dish_path + '.last_op').read()
+    except:
+        last_op = '0' * 7
+    processed = TimeSeries().range(last_op)
+    f = partial(dir_filter, processed=processed, base=dish_path)
+    todo = list(sorted(filter(f, os.listdir(dish_path))))
+    for serie in todo:
+        pass
+        # last_op = process_serie(dish_path, serie)
+    with open(dish_path + '.last_op', 'w') as fn:
+        fn.write(last_op)
+    return check_finish_state(last_op, dish_info)
+
+def check_finish_state(last_serie):
+    from app import conf
+    import datetime as dt
+    return True
+
+
+def dir_filter(path, processed, base):
+    if not os.path.isdir(base + path):
+        return False
+    if path in processed:
+        return False
+    if path == 'focus':
+        return False
+    return True
