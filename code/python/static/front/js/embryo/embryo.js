91a64a75c88848cb33fccca2de44a3d8421f4021
liudm1 <985245771@qq.com>
2018-07-23 11:18:48 +0800

    套用最新胚胎视图页面

--- code/python/static/front/js/embryo/embryo.js
+++ code/python/static/front/js/embryo/embryo.js
@@ -53,13 +53,8 @@ layui.use(['form', 'jquery', 'laydate', 'table', 'layer'], function () {
             var site = document.getElementById('site');
             var siteItem = document.getElementById('siteitem');
             siteItem.style.width = (site.offsetWidth - 44) + "px";
-
         }
         site();
-        $(window).resize(function () {
-            scroll();
-            site();
-        });
 
         // 点击切换样式
         $('#myscrollboxul li').click(function () {
@@ -71,7 +66,12 @@ layui.use(['form', 'jquery', 'laydate', 'table', 'layer'], function () {
             $('#siteitem li').removeClass('active');
             $(this).addClass('active');
         });
-
+		// z轴点击样式
+		$('.time-vertical li').click(function () {
+			$('.time-vertical li').removeClass('active');
+			$(this).addClass('active');
+		});
+		
 
         // 选为最清晰的值提示
         form.on('checkbox(clear)', function (data) {
@@ -101,7 +101,7 @@ layui.use(['form', 'jquery', 'laydate', 'table', 'layer'], function () {
             }
 
         });
- 
+
         // 播放暂停按钮的切换
 
         //上一张 下一张
@@ -134,43 +134,39 @@ layui.use(['form', 'jquery', 'laydate', 'table', 'layer'], function () {
         })
 
         // 滚动效果
-        function showText() {
-            n = n + 1;
-        }
-        setInterval(showText, 200);		
-		
-		function run() {
-			if (n < $(".lg-img img").length) {
-				n = n;
-			} else {
-				n = 0
-			}
-			$(".lg-img img").hide();
-			$(".lg-img img:eq(" + n + ")").show();
-		}
-		var timer = setInterval(run, 200);
-		
-		
-                $('#playBtn').click(function () {
-                    if ($(this).hasClass('play')) {
-                        $(this).removeClass('play');
-                        $(this).addClass('stop');
-						console.log("播放");
-						timer
-						
-						
+        var textTime;
+        var imgTime;
+        // 点击播放暂停
+        $('#playBtn').click(function () {
+            if ($(this).hasClass('play')) {
+                $(this).removeClass('play');
+                $(this).addClass('stop');
+                console.log("播放");
+
+                function showText() {
+                    n = n + 1;
+                }
+                textTime = setInterval(showText, 200);
+
+                function run() {
+                    if (n < $(".lg-img img").length) {
+                        n = n;
                     } else {
-                        $(this).removeClass('stop');
-                        $(this).addClass('play');
-						console.log("暂停");
-						clearInterval(timer);
+                        n = 0
                     }
-                })
-        
+                    $(".lg-img img").hide();
+                    $(".lg-img img:eq(" + n + ")").show();
+                }
+                imgTime = setInterval(run, 200);
 
-
-
-
+            } else {
+                $(this).removeClass('stop');
+                $(this).addClass('play');
+                console.log("暂停");
+                clearInterval(textTime);
+                clearInterval(imgTime);
+            }
+        })
         // 标记结局
         $(".tool-end").on('click', 'li[data-end]', function () {
             var indexData = $(this).attr('data-end');
@@ -186,184 +182,120 @@ layui.use(['form', 'jquery', 'laydate', 'table', 'layer'], function () {
                 $(".mark i").attr('class', "");
                 layer.msg("已取消标注")
             }
-
-
-
         })
-        
-        //初始化radio通用方法
-		function chushihua(dictClass,radioId,divId) {
-			$.ajax({
-				type : "get",
-				url : "/api/v1/dict/list/"+dictClass,
-				datatype : "json",
-				success : function(data) {
-					if (data.code == 0) {
-						var str = "";
-						for (var i = 0; i < data.data.length; i++) {
-						 
-								str+="<input type='radio' name='"+radioId+"' lay-filter="+radioId+" value='" + data.data[i].dictKey + "' title='"+data.data[i].dictValue+"' />";
-						 
-						}
-						$("#"+divId).html(str);
-						form.render();
-					} else {
-						layer.alert(data.msg);
-					}
-				},
-				error : function(request) {
-					layer.alert(request.responseText);
-				}
-			});
+		var canvas = document.getElementById('canvas'); // 得到画布
+		var ctx = canvas.getContext('2d'); // 得到画布的上下文对象
+		var flag = false;
+		var x = 0; // 鼠标开始移动的位置X
+		var y = 0; // 鼠标开始移动的位置Y
+		var url = ''; // canvas图片的二进制格式转为dataURL格式
+		
+		function canvasWidth(){
+			var embr = document.getElementById('embryo');
+			var canvasBox = document.getElementById('canvasBox');
+			canvasBox.style.width = embr.offsetWidth;
+			canvas.width = canvasBox.offsetWidth;
+			canvas.height = canvasBox.offsetHeight;
 		}
-        
-        //动态初始化里程碑的值,字典表milestone
-        chushihua("milestone","milestoneId","milestoneIdDiv");
-                
-        //初始化PN数，字典值pn
-        chushihua("pn","pnId","pnIdDiv");
+		canvasWidth();
 		
-		//初始化均匀度,字典even
-		chushihua("even","evenId","evenIdDiv");
+		/* 为canvas绑定mouse事件 */
 		
-		//初始化碎片率，字典fragment
-		chushihua("fragment","fragmentId","fragmentIdDiv");
-		
-		//初始化评分,字典grade
-		chushihua("grade","gradeId","gradeIdDiv");
-
-	
-		
-		form.on('radio(milestoneId)', function(data){
-			var title = data.elem.title;
-			var value = data.value;
-			$("#stageId").html("("+title+")");
-			//根据不同的value显示不同的胚胎形态
-			if(value=="1") {//PN
-				$("#countDiv").hide();
-				$("#evenDiv").hide();
-				$("#fragmentDiv").hide();
-				$("#gradeDiv").hide();
-				$("#pnDiv").show();
-			}else if(value=="2") {//2C
-				$("#countDiv").show();
-				$("#evenDiv").show();
-				$("#fragmentDiv").hide();
-				$("#gradeDiv").hide();
-				$("#pnDiv").hide();
-			}else if(value=="3" || value=="4" || value=="5" || value=="6") {
-				$("#countDiv").show();
-				$("#evenDiv").show();
-				$("#fragmentDiv").show();
-				if(value=="6") {
-					$("#gradeDiv").show();
-				}else {
-					$("#gradeDiv").hide();
-				}
-				$("#pnDiv").hide();
-			}else {
-				$("#pnDiv").hide();
-				$("#countDiv").hide();
-				$("#evenDiv").hide();
-				$("#fragmentDiv").hide();
-				$("#gradeDiv").hide();
-				layer.alert("待确认");
+		$(".tool-metrical li").click(function(){
+			$('.tool-metrical li').removeClass('active');
+			$(this).addClass('active');
+			if($(this).hasClass('straight')){
+				$('canvas').mousedown(function(e){
+					flag = true;
+					x = e.offsetX; // 鼠标落下时的X
+					y = e.offsetY; // 鼠标落下时的Y
+					console.log(x,y)
+				}).mouseup(function(e){
+					flag = false;
+					url = $('canvas')[0].toDataURL(); // 每次 mouseup 都保存一次画布状态
+					x = e.offsetX; // 鼠标落下时的X
+					y = e.offsetY; // 鼠标落下时的Y
+					console.log(x,y)
+				}).mousemove(function(e){
+					drawLine(e); // 绘制方法
+				});
+			}else{
+				
+				$('canvas').mousedown(function(e){
+					flag = true;
+					x = e.offsetX; // 鼠标落下时的X
+					y = e.offsetY; // 鼠标落下时的Y
+					console.log(x,y)
+				}).mouseup(function(e){
+					flag = false;
+					url = $('canvas')[0].toDataURL(); // 每次 mouseup 都保存一次画布状态
+					x = e.offsetX; // 鼠标落下时的X
+					y = e.offsetY; // 鼠标落下时的Y
+					console.log(x,y)
+				}).mousemove(function(e){
+					drawCircle(e); // 绘制方法
+				});
+				
 			}
 			
 			
+			
+			
+		})
+		
+		$('canvas').mousedown(function(e){
+			flag = true;
+			x = e.offsetX; // 鼠标落下时的X
+			y = e.offsetY; // 鼠标落下时的Y
+			console.log(x,y)
+		}).mouseup(function(e){
+			flag = false;
+			url = $('canvas')[0].toDataURL(); // 每次 mouseup 都保存一次画布状态
+			x = e.offsetX; // 鼠标落下时的X
+			y = e.offsetY; // 鼠标落下时的Y
+			console.log(x,y)
+		}).mousemove(function(e){
+			drawLine(e); // 绘制方法
 		});
 		
-		//保存里程碑
-		$("#saveMilestoneButton").click(function(){
-			var value = $('[name=milestoneId]:checked').val();
-			if(value==undefined) {
-				layer.alert("里程碑节点不能为空,请选择里程碑节点!");
-				return;
-			}else if(value=="1") {//PN
-				var pnId = $("input[name='pnId']:checked").val();
-				if(pnId==undefined) {
-					layer.alert("PN数不能为空,请选择PN数!");
-					return;
-				}
-			}else if(value=="2" || value=="3" || value=="4" || value=="5" || value=="6" ) {//2C
-				var count = $("input[name='count']:checked").val();
-				if(count==undefined) {
-					layer.alert("细胞个数不能为空,请选择细胞个数!");
-					return;
-				}
-				
-				var evenId = $("input[name='evenId']:checked").val();
-				if(evenId==undefined) {
-					layer.alert("均匀度不能为空,请选择均匀度!");
-					return;
-				}
-				
-				if(value=="3" || value=="4" || value=="5" || value=="6") {
-					var fragmentId = $("input[name='fragmentId']:checked").val();
-					if(fragmentId==undefined) {
-						layer.alert("碎片率不能为空,请选择碎片率!");
-						return;
-					}
-				}
-				
-				if(value=="6") {
-					var gradeId = $("input[name='gradeId']:checked").val();
-					if(gradeId==undefined) {
-						layer.alert("评级不能为空,请选择评级!");
-						return;
-					}
-				}
-			}else {
-			 
+		// 画圆
+		function drawCircle(e){
+			if(flag){
+				ctx.clearRect(0,0,canvas.width,canvas.height);
+				ctx.beginPath();
+				var rx = (e.offsetX-x)/2;
+				var ry = (e.offsetY-y)/2;
+				var r = Math.sqrt(rx*rx+ry*ry);
+				ctx.arc(rx+x,ry+y,r,0,Math.PI*2); // 第5个参数默认是false-顺时针
+				ctx.stroke();
+				ctx.strokeStyle ="#ffee19" ; 
+				ctx.lineWidth = 2; 
 			}
-			
-
-			
-			var diameter = $("#diameter").val();
-			if(diameter=="") {
-				layer.alert("直径不能为空,请输入直径!");
-				return;
+		}
+		
+		// 画直线
+		function drawLine(e){
+			if(flag){
+				ctx.clearRect(0,0,canvas.width,canvas.height);
+				ctx.beginPath();
+				ctx.moveTo(x,y);
+				ctx.lineTo(e.offsetX,e.offsetY);
+				ctx.stroke();
+				ctx.strokeStyle ="#ffee19" ; 
+				ctx.lineWidth = 2; 
 			}
-			
-			var area = $("#area").val();
-			if(area=="") {
-				layer.alert("面积不能为空,请选择评级!");
-				return;
-			}
-			
-			var thickness = $("#thickness").val();
-			if(thickness=="") {
-				layer.alert("透明带厚度不能为空,请选择评级!");
-				return;
-			}
-			
-			
-			$.ajax({
-				cache : false,
-				type : "post",
-				url : "/api/v1/milestone/add",
-				data : $('#milestoneForm').serialize(),// 你的formid
-				async : false,
-				error : function(request) {
-					layer.alert(request.responseText);
-				},
-				success : function(data) {
-					if (data.code == 0) {
-						layer.alert("设置成功!");
-					} else {
-						layer.alert(data.msg);
-					}
-					
-				}
-			});
+		}
+		
+		
+		
+		
+		// 跟随屏幕改变的设定	
+		$(window).resize(function () {
+			scroll();
+			site();
+			canvasWidth();
 		});
 		
-	 
-        
+		
     });
-    
-
 })
-
-
-
