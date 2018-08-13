bb4ec8f9b9a4e1e553ca3e9b4762d60955cdd8be
luotengke <luotengke@qq.com>
2018-08-13 14:15:27 +0800

    培养箱视图添加 incubator.html

--- code/html/js/incubator.js
+++ code/html/js/incubator.js
@@ -12,4 +12,18 @@ layui.use(['form', 'jquery', 'laydate', 'table', 'layer', 'element'], function (
 			$(this).addClass('active');
 		}
 	})
+	
+	// 查看病历
+	$('.patient-info').hover(function(){
+		const txt ="无";
+		const div = "<div class='case-details'><dl><dt><strong>姓名</strong><span>"+txt+"</span></dt>"+
+		"<dt><strong>授精时间</strong><span>"+txt+"</span></dt>"+
+		"<dt><strong>首次拍照时间</strong><span>"+txt+"</span></dt>"+
+		"<dt><strong>年龄</strong><span>"+txt+"</span></dt>"+
+		"<dt><strong>授精方式</strong><span>"+txt+"</span></dt>"+
+		"<dt><strong>阶段</strong><span>"+txt+"</span></dt>"+"</dl></div>";
+		$(this).append(div)
+	},function(){
+		$(this).children('div').remove()
+	})
 })
