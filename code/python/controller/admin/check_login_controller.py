350b5dee1a11d2affa8b047c11c89c762a9f07f6
liuyz <liuyz@DESKTOP-LGG34F6.mshome.net>
2018-10-05 22:14:22 +0800

    由于部分代码被冲突，重新补回

--- code/python/controller/admin/check_login_controller.py
+++ code/python/controller/admin/check_login_controller.py
@@ -15,6 +15,6 @@ url_prefix = '/'
 @check_login_controller.route('/', methods=['GET'])
 def main():
     if current_user.is_authenticated:
-        return render_template('front/procedure/procedure.html',htmlType="incubator")
+        return render_template('front/home.html',htmlType="incubator")
     return render_template('login.html')
 
