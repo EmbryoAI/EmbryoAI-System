8f698f0e5bfd006c84dabf6fd961c04d0f70cf10
wangying <yingge_gzb@qq.com>
2019-01-06 15:38:51 +0800

    浙江省人民医院现场改动

--- code/sql/embryoai_system_db_1_0.sql
+++ code/sql/embryoai_system_db_1_0.sql
@@ -17,6 +17,10 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE DATABASE `embryoai_system_db`;
+
+USE `embryoai_system_db`;
+
 -- ----------------------------
 -- Table structure for sys_cell
 -- ----------------------------
