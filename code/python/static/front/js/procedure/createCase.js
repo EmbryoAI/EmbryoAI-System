b729b78f837373c0b6e27348f3cca477d8653311
yangcheng.ai <yangcheng.ai@lanwon.com>
2019-03-19 15:22:44 +0800

    修改身份证验证为弱验证,并验证不通过不能创建病例
    Signed-off-by: yangcheng.ai <yangcheng.ai@lanwon.com>

--- code/python/static/front/js/procedure/createCase.js
+++ code/python/static/front/js/procedure/createCase.js
@@ -114,6 +114,11 @@ layui.use(['form', 'jquery', 'laydate', 'table', 'layer', 'element','address'], 
 
 
 function addCase(){
+	var checkIdResult = $("#checkIdResult").val();
+	if(checkIdResult == "" || checkIdResult == 'false'){
+		layer.alert('请输入正确的身份证号!');
+		return;
+	}
 	var selected = $("#catalogSelect  option:selected").text();
 	if(selected == 0){
 		layer.alert('采集目录不能为空!');
@@ -171,11 +176,13 @@ function countAge(){
 //根据身份证号计算年龄
 function countAgeByIdCard(){
 	var idCard = $('#nope').val(); 
-	var checkResult = IdentityCodeValid(idCard);
+	var checkResult = isCardNo(idCard);
+	$("#checkIdResult").val(checkResult);
 	if(checkResult == false){
 		layer.alert("请输入正确的身份证!");  
 		return  false;  
 	}
+	
 	var birthDate = idCard.substring(6, 14);
 	var year = birthDate.substring(0,4);
 	var month =  birthDate.substring(4,6);
@@ -192,47 +199,22 @@ function countAgeByIdCard(){
 }
 
 
-function IdentityCodeValid(code) { 
-	var city={11:"北京",12:"天津",13:"河北",14:"山西",15:"内蒙古",21:"辽宁",22:"吉林",23:"黑龙江 ",31:"上海",
-			32:"江苏",33:"浙江",34:"安徽",35:"福建",36:"江西",37:"山东",41:"河南",42:"湖北 ",43:"湖南",44:"广东",
-			45:"广西",46:"海南",50:"重庆",51:"四川",52:"贵州",53:"云南",54:"西藏 ",61:"陕西",62:"甘肃",63:"青海",
-			64:"宁夏",65:"新疆",71:"台湾",81:"香港",82:"澳门",91:"国外 "};
-	var tip = "";
-	var pass= true;
-
-	if(!code || !/^\d{6}(18|19|20)?\d{2}(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])\d{3}(\d|X)$/i.test(code)){
-		tip = "身份证号格式错误";
-		pass = false;
+function isCardNo(id) {
+	var format = /^(([1][1-5])|([2][1-3])|([3][1-7])|([4][1-6])|([5][0-4])|([6][1-5])|([7][1])|([8][1-2]))\d{4}(([1][9]\d{2})|([2]\d{3}))(([0][1-9])|([1][0-2]))(([0][1-9])|([1-2][0-9])|([3][0-1]))\d{3}[0-9xX]$/;
+	//号码规则校验
+	if (!format.test(id)) {
+		return false;
 	}
-
-   else if(!city[code.substr(0,2)]){
-		tip = "地址编码错误";
-		pass = false;
+	//区位码校验
+	//出生年月日校验   前正则限制起始年份为1900;
+	var year = id.substr(6, 4),//身份证年
+		month = id.substr(10, 2),//身份证月
+		date = id.substr(12, 2),//身份证日
+		time = Date.parse(month + '-' + date + '-' + year),//身份证日期时间戳date
+		now_time = Date.parse(new Date()),//当前时间戳
+		dates = (new Date(year, month, 0)).getDate();//身份证当月天数
+	if (time > now_time || date > dates) {
+		return false;
 	}
-	else{
-		//18位身份证需要验证最后一位校验位
-		if(code.length == 18){
-			code = code.split('');
-			//∑(ai×Wi)(mod 11)
-			//加权因子
-			var factor = [ 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 ];
-			//校验位
-			var parity = [ 1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2 ];
-			var sum = 0;
-			var ai = 0;
-			var wi = 0;
-			for (var i = 0; i < 17; i++)
-			{
-				ai = code[i];
-				wi = factor[i];
-				sum += ai * wi;
-			}
-			var last = parity[sum % 11];
-			if(parity[sum % 11] != code[17]){
-				tip = "校验位错误";
-				pass =false;
-			}
-		}
-	}
-	return pass;
+	return true;
 }
