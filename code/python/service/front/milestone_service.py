d889e6c7a4c341107c2e6da47489f3a6d809fbb8
liuyz <liuyz@DESKTOP-LGG34F6.mshome.net>
2019-03-21 18:36:01 +0800

    增加保存里程碑的异常打印！

--- code/python/service/front/milestone_service.py
+++ code/python/service/front/milestone_service.py
@@ -12,6 +12,7 @@ from common import uuid
 import re
 import time
 import datetime
+from traceback import print_exc
 from app import current_user,conf
 import service.front.image_service as image_service
 
@@ -176,6 +177,7 @@ def insertMilestone(request):
 #         print(sumScore)
 #         embryo_mapper.updateEmbryoScore(embryoId,sumScore)
     except:
+        print_exc()
         return 400, '设置里程碑时异常!'
     
     #读取上传云端代码块开关
